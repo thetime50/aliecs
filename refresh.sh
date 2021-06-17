2bf8bc8de141b355fcde2c02084bdc6e4fde64de
TheTime50 <1004763777@qq.com>
2021-06-17 17:55:17 +0800

    echo `pwd`

--- refresh.sh
+++ refresh.sh
@@ -2,34 +2,34 @@ base_path="/home/thetime/project"
 
 cd $base_path"/aliecs"
 git pull
-echo `aliecs`
+echo `pwd`
 echo -
 
 cd $base_path"/io"
 git pull
 # cp -f -r /home/zero/thetime50/io/. /home/zero/thetime50
 cp -rf `ls -a | grep -vE ".git|^\.$|^\.\.$" | xargs` $base_path
-echo `io`
+echo `pwd`
 echo -
 
 cd $base_path"/Bilebilelike"
 git pull
-echo `Bilebilelike`
+echo `pwd`
 echo -
 
 cd $base_path"/front-laboratory"
 git pull
-echo `front-laboratory`
+echo `pwd`
 echo -
 
 cd $base_path"/front-server-laboratory"
 git pull
-echo `front-server-laboratory`
+echo `pwd`
 echo -
 
 cd $base_path"/back-laboratory"
 git pull
-echo `back-laboratory`
+echo `pwd`
 echo -
 
 echo -
