d1103ee4ab1203c2d3803113a513a64fc9701518
TheTime50 <1004763777@qq.com>
2021-06-17 17:22:02 +0800

    refresh.hs

---
+++ refresh.sh
@@ -0,0 +1,30 @@
+cd /home/zero/thetime50/io
+git pull
+# cp -f -r /home/zero/thetime50/io/. /home/zero/thetime50
+cp -rf `ls -a | grep -vE ".git|^\.$|^\.\.$" | xargs` /home/zero/thetime50
+echo `pwd`
+echo -
+
+cd /home/zero/thetime50/Bilebilelike
+git pull
+echo `pwd`
+echo -
+
+cd /home/zero/thetime50/front-laboratory
+git pull
+echo `pwd`
+echo -
+
+cd /home/zero/thetime50/front-server-laboratory
+git pull
+echo `pwd`
+echo -
+
+# cd /home/zero/back-laboratory
+# git pull
+# echo `pwd`
+# echo -
+
+echo -
+echo -- $(date +%Y-%m-%d %H:%m:%s) --
+echo -
